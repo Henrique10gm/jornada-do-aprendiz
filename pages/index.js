9eca7650bf93548be1540c5e862c1eacc06b9202
Henrique Montengro <152886040+Henrique10gm@users.noreply.github.com>
2024-02-22 21:44:30 +0000

    Adiciona scripts `Lint:check:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
 function Home() {
-    return <h1>Oi mamãe, você é a melhor mãe do mundo! 
-        Esse é meu primeiro espaço na internet, deu um trabalhão
+  return (
+    <h1>
+      Oi mamãe, você é a melhor mãe do mundo! Esse é meu primeiro espaço na
+      internet, deu um trabalhão{" "}
     </h1>
+  );
+}
+
+function Teste() {
+  console.log("teste");
 }
 
 export default Home;
